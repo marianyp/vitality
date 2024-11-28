db65fc31e1d48cc5f64cb77b6bb211070453b754
Mariany P. <marianypinkston@gmail.com>
2024-11-28 10:20:12 -0500

    Balancing tweaks

--- src/main/java/dev/mariany/vitality/util/VitalityConstants.java
+++ src/main/java/dev/mariany/vitality/util/VitalityConstants.java
@@ -4,8 +4,8 @@ public final class VitalityConstants {
     public static final int HEALTHY_EATING_WINDOW = 10;
     public static final float MIN_BUFF_RATIO = 0.7F;
 
-    public static final int MAX_WALL_JUMPS = 3;
-    public static final int WALL_SLIDE_DELAY = 10;
+    public static final int MAX_WALL_JUMPS = 4;
+    public static final int WALL_SLIDE_DELAY = 16;
     public static final float WALL_JUMP_HEIGHT = 0.5F;
     public static final float WALL_JUMP_EXHAUSTION = 0.8F;
 
